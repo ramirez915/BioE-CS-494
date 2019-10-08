2f446ff631262629df03f08df419d426a8663eb0
bondavalli96 <bondavalli96@users.noreply.github.com>
2019-10-08 16:36:48 -0500

    update

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,12 +1,30 @@
 #include <StopWatch.h>
 
+float force=[];
+
+int f_s0=A0;
+int f_s1=A1;
+int f_s2=A2;
+int f_s3=A3;
+
 void setup() {
-  // put your setup code here, to run once:
+  // initialize the serial communication:
   Serial.begin(115200);
-  
+  //outputs for the leds
+  pinMode(10, OUTPUT); 
+  pinMode(11, OUTPUT); 
+  //analogue inputs from FRSs
+  pinMode(A0, INPUT); 
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT); 
+  pinMode(A3, INPUT); 
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //xfgh
+  
+    force[i]=analogRead(f_s0);
+    fore
+    
+  }
 }
