209ea645ac8a5f1ea5ef17e24fbdc614fdf8e600
ramirez915 <jose.angelramirez915@gmail.com>
2019-10-08 16:55:09 -0500

    update

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,16 +1,17 @@
 #include <StopWatch.h>
 
-float force=[];
+float force[4];
+float mappedForce[4];
 
 int f_s0=A0;
 int f_s1=A1;
 int f_s2=A2;
 int f_s3=A3;
 
-int led_1=10;
+int led_0=10;
 int led_1=11;
-int led_1=12;
-int led_1=13;
+int led_2=12;
+int led_3=13;
 
 void setup() {
   // initialize the serial communication:
@@ -29,24 +30,30 @@ void setup() {
 }
 
 void loop() {
-  
-    force[0]=analogRead(f_s0);
-    force[1]=analogRead(f_s1);
-    force[2]=analogRead(f_s2);
-    force[3]=analogRead(f_s3);
+  force[0]=analogRead(f_s0);
+  force[1]=analogRead(f_s1);
+  force[2]=analogRead(f_s2);
+  force[3]=analogRead(f_s3);
 
-    //mapping force_inputs with leds_outputs
-
-    for(i=1
-
-
-
-
-
-    
-    digitalWrite(LED13,HIGH);
-
-    digitalw
-    
+  //mapping force_inputs with leds_outputs
+  for(i=0; i< 4; i++){
+    mappedforce[i] = map(force[i]-9,0,1023-9,0,255);
   }
+  analogWrite(led_0,mappedForce[0]);
+  analogWrite(led_1,mappedForce[1]);
+  analogWrite(led_2,mappedForce[2]);
+  analogWrite(led_3,mappedForce[3]);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(A0));
+ // Serial.print(" ");
+  
+ // Serial.println(mapped);
+ // Serial.print(" ");
+  analogWrite(3,mapped);
+  
+  delay(50);
 }
