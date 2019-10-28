b809006f43e937e05692951820de5123e4f314ff
bondavalli96 <bondavalli96@users.noreply.github.com>
2019-10-28 18:25:21 -0500

    up

--- Lab2/GettingStartedProject/GettingStartedProject.ino
+++ Lab2/GettingStartedProject/GettingStartedProject.ino
@@ -22,11 +22,11 @@
 
 //  Variables
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
-int LED13 = 11;   //  The on-board Arduion LED
+int LED11 = 11;   //  The on-board Arduion LED
 
 
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
-int Threshold = 670;            // Determine which Signal to "count as a beat", and which to ingore.
+int thr = 600;            // Determine which Signal to "count as a beat", and which to ingore.
 
 
 // The SetUp Function:
@@ -36,20 +36,106 @@ void setup() {
 
 }
 
+
+void acquire_signal() {
+
+  //heart rate acquisition
+  //check for signal acquisition
+  //pins are D11=LO- and D09=LO+
+
+  float seg;
+  float R_R;
+//
+// 
+//  // subtract the last reading:
+//  total_bpm = total_bpm - readings_bpm[readIndex_bpm];
+//  // read from the sensor:
+//  readings_bpm[readIndex_bpm] = analogRead(A0);
+//  /*Serial.print("analogueR: ");
+//  Serial.println(analogRead(A0));
+//  Serial.print("READS: ");
+//  Serial.println(readings[readIndex_bpm]);
+//  */
+//  // add the reading to the total:
+//  total_bpm = total_bpm + readings_bpm[readIndex_bpm];
+//  // advance to the next position in the array:
+//  readIndex_bpm = readIndex_bpm + 1;
+//
+//  // if we're at the end of the array...
+//  if (readIndex_bpm >= numReadings_bpm) {
+//    // ...wrap around to the beginning:
+//    readIndex_bpm = 0;
+//  }
+//
+//  // calculate the average:
+//  average_bpm = total_bpm / numReadings_bpm;
+//  //Serial.print("AVG ");
+//   //Serial.println(average);
+//
+//seg=average_bpm;
+
+
+//if(seg<thr) {
+//
+//  upper=0;
+//}
+//Serial.print("Segnal:");
+//Serial.println(seg);
+//Serial.print(" ");
+
+   // seg=analogRead(A0);
+//
+ //Serial.println(seg);
+    //check for threshold
+   
+//    if(seg>thr && upper==0){
+
+seg=analogRead(PulseSensorPurplePin);
+Serial.println(seg); 
+
+if(seg>thr) {
+       digitalWrite(LED11,HIGH);
+       
+       //R-peak detected, save time instant
+      //t must be current time
+      bpm_timer.stop();
+      long bpmTimer = bpm_timer.value();
+       R_R=bpmTimer;
+     // R_R= (bpmTimer/float(10));
+      //Serial.print("R_R:");
+     //Serial.println(R_R);
+      //Serial.print(" ");
+      bpm_timer.reset();
+      bpm_timer.start();
+      //compute bpm as a frequency
+      bpm=float(60)/(R_R/1000);
+      serialOutputWhenBeatHappens();
+    } 
+
+    else{
+      digitalWrite(LED,LOW); 
+    }
+
+send
+
+    delay(30);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // The Main Loop Function
 void loop() {
 
-  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
-                                              // Assign this value to the "Signal" variable.
-
-   Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
-
-
-   if(Signal > Threshold){                        // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
-     digitalWrite(LED13,HIGH);
-   } else {
-     digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
-   }
 
 
 delay(10);
